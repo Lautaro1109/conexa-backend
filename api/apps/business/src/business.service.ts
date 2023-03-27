b292eb04a2768f4c59f1762f801cf0a457721f4e
Lautaro1109 <lautyriveros@hotmail.com>
2023-03-27 17:41:51 -0300

    [Feat] : Adding all logic to both ms.

--- api/apps/business/src/business.service.ts
+++ api/apps/business/src/business.service.ts
@@ -1,8 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { UserListDto } from './dto/userList.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { UserDocument, User } from '../../../apps/login/src/schema/user.schema';
 
 @Injectable()
 export class BusinessService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(
+    @InjectModel(User.name) private readonly userModel: Model<UserDocument>,
+  ) {}
+
+  async getUserList(payload: UserListDto): Promise<User[]> {
+    const { page, limit, search } = payload;
+    const skip = page * limit;
+
+    const query = search ? { mail: { $regex: search, $options: 'i' } } : {};
+
+    return this.userModel.find(query).skip(skip).limit(limit).exec();
   }
 }
